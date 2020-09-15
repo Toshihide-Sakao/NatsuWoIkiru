b86055d01949b1c6840a178d40c9921661a081ed
Toshihide-Sakao <toshihide.sakao@elev.ga.ntig.se>
2020-09-16 05:44:15 +0900

    ok nice

---
+++ Kiai3d.cs
@@ -0,0 +1,23 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Storyboarding3d;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class Kiai3d : StoryboardObjectGenerator
+    {
+        public override void Generate()
+        {
+            
+        }
+    }
+}
